b764fe57f503879a07aeca846a2572309931cefc
bmiller52 <millerbam52@gmail.com>
2016-12-07 16:44:03 -0500

    Updateing files

--- dungeon_game-git.py
+++ dungeon_game-git.py
@@ -2,117 +2,151 @@ import random
 import sys
 from sys import exit
 
-player_trail = []
+def start_game():
+    player_trail = []
 
-if_sword = [0]
+    if_sword = [0]
 
-cell = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
-        (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
-        (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6),
-        (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6),
-        (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6),
-        (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)]
+    cell = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
+            (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
+            (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6),
+            (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6),
+            (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6),
+            (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)]
 
-def right_location():
-    start_location = random.choice(cell)
-    monster_location = random.choice(cell)
-    monster2_location = random.choice(cell)
-    sword_location = random.choice(cell)
-    escape_location = random.choice(cell)
+    def start_again():
+        play_again = raw_input('Do you want to play again? y/n ').lower()
+        if play_again == 'y':
+            start_game()
 
-    if monster2_location == start_location or escape_location == start_location:
-        return right_location()
+        if play_again == 'n':
+            print(' ')
+            print('Ok. Hope you had fun!')
+            print(' ')
+            sys.exit()
 
-    if monster_location == monster2_location:
-        return right_location()
+    def right_location():
+        start_location = random.choice(cell)
+        monster_location = random.choice(cell)
+        monster2_location = random.choice(cell)
+        sword_location = random.choice(cell)
+        escape_location = random.choice(cell)
 
-    if sword_location == start_location or sword_location == monster_location or sword_location == monster2_location or sword_location == escape_location:
-        return right_location()
+        if monster2_location == start_location or escape_location == start_location:
+            return right_location()
 
-    if monster_location == escape_location or monster_location == start_location or monster2_location == escape_location:
-        return right_location()
+        if monster_location == monster2_location:
+            return right_location()
 
-    return start_location, monster_location, monster2_location, sword_location, escape_location
+        if sword_location == start_location or sword_location == monster_location or sword_location == monster2_location or sword_location == escape_location:
+            return right_location()
 
-def print_map():
+        if monster_location == escape_location or monster_location == start_location or monster2_location == escape_location:
+            return right_location()
 
-  count = 0
-  printed_map = []
+        return start_location, monster_location, monster2_location, sword_location, escape_location
 
-  while count < len(cell):
-    current_room = cell[count]
+    def print_map():
 
-    if current_room == player_location:
-		printed_map.append('X|')
+      count = 0
+      printed_map = []
 
-    elif current_room in player_trail:
-		printed_map.append('.|')
+      while count < len(cell):
+        current_room = cell[count]
 
-    else:
-		printed_map.append(' |')
+        if current_room == player_location:
+    		printed_map.append('X|')
 
-    if current_room[1] == 6:
-		printed_map.append('\n')
-    count += 1
+        elif current_room in player_trail:
+    		printed_map.append('.|')
 
-  print(''.join(printed_map))
+        else:
+    		printed_map.append('_|')
 
-player_location, monster_location, monster2_location, sword_location, escape_location = right_location()
+        if current_room[1] == 6:
+    		printed_map.append('\n')
+        count += 1
 
-print(' ')
-print('Wlecome to the Dungeon game!')
-print('You are in a 6 by 6 grid. There are 2 monsters, one sword, and one escape.')
-print('Simply type in (W) UP, (D) RIGHT, (S) DOWN, or (A) LEFT. Then press enter')
-print(' ')
+      print(''.join(printed_map))
 
-player_trail.append(player_location)
+    player_location, monster_location, monster2_location, sword_location, escape_location = right_location()
 
-print_map()
-
-while True:
-
-    move = raw_input('Which way do you want to move? ').upper()
-
-    if move == 'D' and player_location[1] < 6:
-        player_location = player_location[0], player_location[1] + 1
-
-    elif move == 'W' and player_location[0] > 1:
-        player_location = player_location[0] - 1, player_location[1]
-
-    elif move == 'A' and player_location[1] > 1:
-        player_location = player_location[0], player_location[1] - 1
-
-    elif move == 'S' and player_location[0] < 6:
-        player_location = player_location[0] + 1, player_location[1]
-
-    if player_location == sword_location:
+    def help_info():
         print(' ')
-        print('Great! You have found the sword')
-        print('Now if you run into a monster you can slay it')
+        print('Controls: (W) UP, (D) RIGHT, (S) DOWN, (A) LEFT.')
+        print(' ')
+        print('You are in a dark scary dungeon and there\'s only one escape')
+        print('There\'s 2 monsters and one sword.')
         print(' ')
 
-        if_sword[0] = 1
-
-    if player_location == monster_location or player_location == monster2_location:
-        if 0 in if_sword:
-            print('Oh no! The Monster got you!')
-            print(' ')
-            break
-
-        if 1 in if_sword:
-            print(' ')
-            print('Hooray! You have just killed the monster')
-            print('But you lost the sword')
-            print(' ')
-
-            if_sword[0] = 0
-
-    if player_location == escape_location:
-        print('Yay! You escaped!')
-        print(' ')
-        break
+    print(' ')
+    print('Wlecome to the Dungeon game!')
+    print('You are in a 6 by 6 grid. There are 2 monsters, one sword, and one escape.')
+    print('Simply type in (W) UP, (D) RIGHT, (S) DOWN, or (A) LEFT. Then press enter')
+    print(' ')
 
     player_trail.append(player_location)
 
-    print(' ')
     print_map()
+
+    while True:
+
+        print('(Q) for quit, (H) for help')
+        move = raw_input('Which way do you want to move? ').upper()
+
+        if move == 'D' and player_location[1] < 6:
+            player_location = player_location[0], player_location[1] + 1
+
+        elif move == 'W' and player_location[0] > 1:
+            player_location = player_location[0] - 1, player_location[1]
+
+        elif move == 'A' and player_location[1] > 1:
+            player_location = player_location[0], player_location[1] - 1
+
+        elif move == 'S' and player_location[0] < 6:
+            player_location = player_location[0] + 1, player_location[1]
+
+        if move == 'Q':
+            print(' ')
+            print('Thanks for playing!')
+            print(' ')
+            sys.exit()
+
+        if move == 'H':
+            help_info()
+            continue
+
+        if player_location == sword_location:
+            print(' ')
+            print('Great! You have found the sword')
+            print('Now if you run into a monster you can slay it')
+            print(' ')
+
+            if_sword[0] = 1
+
+        if player_location == monster_location or player_location == monster2_location:
+            if 0 in if_sword:
+                print('Oh no! The Monster got you!')
+                print(' ')
+                start_again()
+
+            if 1 in if_sword:
+                print(' ')
+                print('Hooray! You have just killed the monster')
+                print('But you lost the sword')
+                print(' ')
+
+                if_sword[0] = 0
+
+        if player_location == escape_location:
+            print(' ')
+            print('Yay! You escaped!')
+            print(' ')
+            start_again()
+
+        player_trail.append(player_location)
+
+        print(' ')
+        print_map()
+
+start_game()
