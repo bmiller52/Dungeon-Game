190bc54e7e3abeaaaac5720759e84813bb2e8f71
bmiller52 <millerbam52@gmail.com>
2016-12-07 11:28:59 -0500

    updateing files

--- dungeon_game-git.py
+++ dungeon_game-git.py
@@ -6,12 +6,6 @@ player_trail = []
 
 if_sword = [0]
 
-class bcolors:
-	blue = '\033[94m'
-	red = '\033[91m'
-	green = '\033[92m'
-	grey = '\033[90m'
-
 cell = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
         (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
         (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6),
@@ -23,23 +17,22 @@ def right_location():
     start_location = random.choice(cell)
     monster_location = random.choice(cell)
     monster2_location = random.choice(cell)
-    monster3_location = random.choice(cell)
     sword_location = random.choice(cell)
     escape_location = random.choice(cell)
 
-    if monster2_location == start_location or monster3_location == escape_location or monster3_location == start_location or escape_location == start_location:
+    if monster2_location == start_location or escape_location == start_location:
         return right_location()
 
-    if monster_location == monster2_location or monster_location == monster3_location or monster2_location == monster3_location:
+    if monster_location == monster2_location:
         return right_location()
 
-    if sword_location == start_location or sword_location == monster_location or sword_location == monster2_location or sword_location == monster3_location or sword_location == escape_location:
+    if sword_location == start_location or sword_location == monster_location or sword_location == monster2_location or sword_location == escape_location:
         return right_location()
 
     if monster_location == escape_location or monster_location == start_location or monster2_location == escape_location:
         return right_location()
 
-    return start_location, monster_location, monster2_location, monster3_location, sword_location, escape_location
+    return start_location, monster_location, monster2_location, sword_location, escape_location
 
 def print_map():
 
@@ -50,26 +43,26 @@ def print_map():
     current_room = cell[count]
 
     if current_room == player_location:
-      printed_map.append(bcolors.blue + 'X')
+		printed_map.append('X|')
 
     elif current_room in player_trail:
-      printed_map.append(bcolors.blue + '.')
+		printed_map.append('.|')
 
     else:
-      printed_map.append(bcolors.blue + '*')
+		printed_map.append(' |')
 
     if current_room[1] == 6:
-      printed_map.append('\n')
+		printed_map.append('\n')
     count += 1
 
   print(''.join(printed_map))
 
-player_location, monster_location, monster2_location, monster3_location, sword_location, escape_location = right_location()
+player_location, monster_location, monster2_location, sword_location, escape_location = right_location()
 
 print(' ')
-print(bcolors.blue + 'Wlecome to the Dungeon game!')
-print('You are in a 6 by 6 grid. There are 3 monsters, one sword, and one escape.')
-print('Simply type in (U)P, (R)IGHT, (D)OWN, or (L)EFT. Then press enter')
+print('Wlecome to the Dungeon game!')
+print('You are in a 6 by 6 grid. There are 2 monsters, one sword, and one escape.')
+print('Simply type in (W) UP, (D) RIGHT, (S) DOWN, or (A) LEFT. Then press enter')
 print(' ')
 
 player_trail.append(player_location)
@@ -78,44 +71,44 @@ print_map()
 
 while True:
 
-    move = raw_input(bcolors.blue + 'Which way do you want to move? ').upper()
+    move = raw_input('Which way do you want to move? ').upper()
 
-    if move == 'R' and player_location[1] < 6:
+    if move == 'D' and player_location[1] < 6:
         player_location = player_location[0], player_location[1] + 1
 
-    elif move == 'U' and player_location[0] > 1:
+    elif move == 'W' and player_location[0] > 1:
         player_location = player_location[0] - 1, player_location[1]
 
-    elif move == 'L' and player_location[1] > 1:
+    elif move == 'A' and player_location[1] > 1:
         player_location = player_location[0], player_location[1] - 1
 
-    elif move == 'D' and player_location[0] < 6:
+    elif move == 'S' and player_location[0] < 6:
         player_location = player_location[0] + 1, player_location[1]
 
     if player_location == sword_location:
         print(' ')
-        print(bcolors.green + 'Great! You have found the sword')
+        print('Great! You have found the sword')
         print('Now if you run into a monster you can slay it')
         print(' ')
 
         if_sword[0] = 1
 
-    if player_location == monster_location or player_location == monster2_location or player_location == monster3_location:
+    if player_location == monster_location or player_location == monster2_location:
         if 0 in if_sword:
-            print(bcolors.red + 'Oh no! The Monster got you!')
+            print('Oh no! The Monster got you!')
             print(' ')
             break
 
         if 1 in if_sword:
             print(' ')
-            print(bcolors.green + 'Hooray! You have just killed the monster')
+            print('Hooray! You have just killed the monster')
             print('But you lost the sword')
             print(' ')
 
             if_sword[0] = 0
 
     if player_location == escape_location:
-        print(bcolors.green + 'Yay! You escaped!')
+        print('Yay! You escaped!')
         print(' ')
         break
 
